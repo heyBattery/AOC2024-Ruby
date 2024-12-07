8949b0c3e00b6bfe7d45ba942d98a74119c38046
Battery <heybattery.email@gmail.com>
2024-12-06 21:54:43 -0800

    day 7 main complete

---
+++ 07/main.rb
@@ -0,0 +1,26 @@
+total = 0
+
+def permutations(factors)
+  if factors.length == 2
+    return [factors[0] + factors[1], factors[0] * factors[1]]
+  else
+    tail = factors.pop
+    possible_values = permutations(factors)
+    return possible_values.map {|val| [val+tail, val*tail]}.flatten
+  end
+end
+
+
+File.foreach('input.txt').with_index do |line, linenum|
+  values = line.split(':')
+  target = values[0].to_i
+  factors = values[1].chomp.split.map {|str| str.to_i}
+  # puts "targeting #{target} using #{factors}"
+  perms = permutations(factors)
+  # puts "perms #{perms}"
+  if perms.index(target) != nil
+    total += target
+  end
+end
+
+puts "total: #{total}"
